1bfe78ab0bed87f4ff6a123caa2cd08cda270610
eksc <ek_baby@mail.ru>
2021-04-22 09:28:40 +0500

    +Add IProduct and IPagination models; +Add Shop component and Product component.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './shared/models/product';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Store';
+
+  constructor() {}
+
+  ngOnInit(): void {
+   
+  }
+  
 }
